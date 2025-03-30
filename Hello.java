32f56ef8d8f120d8a78021bee94a9e6a5abb9adb
Barry Lu <blu1@leomail.tamuc.edu>
2025-03-30 14:03:10 -0500

    Math changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Test new Barry chanegs");
+
+        int x = 1;
+        int y = 10;
+
+        System.out.println(x + y);
     }
 }
