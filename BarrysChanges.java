2143b5caa36054151cfe9f1902feaba0de9213c9
Barry Lu <blu1@leomail.tamuc.edu>
2025-04-20 18:09:07 -0500

    Was seeing how to get the time.

--- BarrysChanges.java
+++ BarrysChanges.java
@@ -1,5 +1,9 @@
+import java.time.LocalTime;
+
 public class BarrysChanges {
     public static void main(String[] args) {
-        System.out.println("This is my file.");
+        LocalTime myObj = LocalTime.now();
+        int time1 = myObj.getMinute();
+        System.out.println(time1);
     }
 }
